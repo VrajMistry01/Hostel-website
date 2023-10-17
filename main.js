433d48a54ee69a085e3eda1809dd48ece1c44333
Vraj Mistry <108046769+VrajMistry01@users.noreply.github.com>
2023-10-17 15:53:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+list = document.querySelectorAll(".nav-link");
+
+list.forEach((item) => {
+  item.addEventListener("click", () => {
+    list.forEach((li) => li.classList.remove("active"));
+    item.classList.add("active");
+    // Ensure that index is within valid bounds before scrolling
+  });
+});
+// Function to check if an element is in the viewport
+function isElementInViewport(el) {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle the scroll event
+function handleScroll() {
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach((card) => {
+    if (isElementInViewport(card)) {
+      card.classList.add("fly-in"); // Apply the CSS animation class
+    }
+  });
+}
+
+// Attach the scroll event listener
+window.addEventListener("scroll", handleScroll);
+
+// Initial check when the page loads
+handleScroll();
+
+const boxes = document.querySelectorAll(".box");
+// console.log(boxes);
+const checkboxes = () => {
+  const triggerbottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((ele) => {
+    const boxtop = ele.getBoundingClientRect().top;
+    if (boxtop < triggerbottom) {
+      ele.classList.add("show");
+    } else {
+      ele.classList.remove("show");
+    }
+  });
+};
+window.addEventListener("scroll", checkboxes);
+checkboxes();
